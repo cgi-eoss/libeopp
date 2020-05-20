05a86687bdde8bb89b2a739e5d2e9324ece93949
Peter van Zetten <peter.vanzetten@gmail.com>
2020-05-20 19:09:26 +0100

    Add Jobs#getStepInstance utility method
    Change-Id: I8361f485520eaf7832f69fbc99fa06a41a84ec52

--- job/src/main/java/com/cgi/eoss/eopp/job/Jobs.java
+++ job/src/main/java/com/cgi/eoss/eopp/job/Jobs.java
@@ -1,5 +1,7 @@
 package com.cgi.eoss.eopp.job;
 
+import java.util.NoSuchElementException;
+
 /**
  * <p>Utility methods for working with {@link Job} objects.</p>
  */
@@ -7,6 +9,15 @@ public final class Jobs {
 
     private Jobs() {
         // no-op for utility class
+    }
+
+    /**
+     * @return The StepInstance with the given identifier in the given Job.
+     * @throws NoSuchElementException If the Job contains no StepInstance with the given identifier.
+     */
+    public static StepInstance getStepInstance(Job job, String stepIdentifier) {
+        return job.getStepList().stream().filter(it -> it.getIdentifier().equals(stepIdentifier)).findFirst()
+                .orElseThrow(() -> new NoSuchElementException(String.format("Job %s does not contain a step instance with identifier %s", job.getUuid(), stepIdentifier)));
     }
 
 }
