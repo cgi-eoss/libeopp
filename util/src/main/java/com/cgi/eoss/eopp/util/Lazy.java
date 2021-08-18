89c66cf8dfb3b9e14a4e2da5b25e2a4bf69c66c9
Peter van Zetten <peter.van.zetten@cgi.com>
2021-08-18 20:13:06 +0100

    Improve Lazy.lazily with thread-safety, null check, and docs
    Change-Id: Ide2897854abeac00637125921608d3e1e9d9454f

--- util/src/main/java/com/cgi/eoss/eopp/util/Lazy.java
+++ util/src/main/java/com/cgi/eoss/eopp/util/Lazy.java
@@ -16,7 +16,10 @@
 
 package com.cgi.eoss.eopp.util;
 
-import java.util.Optional;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Supplier;
 
 public final class Lazy {
@@ -24,13 +27,40 @@ public final class Lazy {
         // no-op utility class constructor
     }
 
+    /**
+     * <p>Wrap the given {@link Supplier} for lazy, atomic execution. Ensures
+     * the initialisation function is only called once, even if the resulting
+     * Supplier is called multiple times and from multiple threads.</p>
+     * <p>Note that the given Supplier must not return null.</p>
+     *
+     * @param supplier An initialisation function, which is evaluated only
+     *                 once.
+     * @param <Z>      The return type of the initialisation function.
+     * @return A one-time lazy provider for the value produced by the given
+     * Supplier.
+     */
     public static <Z> Supplier<Z> lazily(Supplier<Z> supplier) {
         return new Supplier<Z>() {
-            Z value;
+            private final AtomicReference<Z> value = new AtomicReference<>();
+            private final Lock lock = new ReentrantLock();
 
             @Override
             public Z get() {
-                return Optional.ofNullable(value).orElseGet(() -> value = supplier.get());
+                Z result = value.get();
+                if (result == null) {
+                    lock.lock();
+                    try {
+                        // Another thread may have entered between (result == null) and lock.lock(), so we check again
+                        result = value.get();
+                        if (result != null) {
+                            return result;
+                        }
+                        return Objects.requireNonNull(value.updateAndGet(x -> supplier.get()));
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+                return result;
             }
         };
     }
--- util/src/test/java/com/cgi/eoss/eopp/util/LazyTest.java
+++ util/src/test/java/com/cgi/eoss/eopp/util/LazyTest.java
@@ -20,10 +20,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(JUnit4.class)
 public class LazyTest {
@@ -45,6 +51,40 @@ public class LazyTest {
         assertThat(count.get()).isEqualTo(1);
         assertThat(lazyResult.get()).isEqualTo(result);
         assertThat(count.get()).isEqualTo(1);
+    }
+
+    @Test
+    public void testLazyThreadSafety() throws InterruptedException {
+        String result = "result";
+
+        AtomicInteger count = new AtomicInteger(0);
+        Supplier<String> countingSupplier = () -> {
+            count.addAndGet(1);
+            return result;
+        };
+
+        Supplier<String> lazyResult = Lazy.lazily(countingSupplier);
+
+        assertThat(count.get()).isEqualTo(0);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        Collection<Callable<String>> tasks = Collections.nCopies(100, lazyResult::get);
+        executorService.invokeAll(tasks);
+
+        assertThat(lazyResult.get()).isEqualTo(result);
+        assertThat(count.get()).isEqualTo(1);
+    }
+
+    @Test
+    public void testLazyNull() {
+        Supplier<String> lazyResult = Lazy.lazily(() -> null);
+
+        try {
+            lazyResult.get();
+            fail("Expected NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e).hasMessageThat().isNull();
+        }
     }
 
 }
